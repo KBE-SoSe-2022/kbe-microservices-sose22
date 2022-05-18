ddb7f7410130ef289669a2c0ce4964c5ef1cb5f1
Ostervogel <m.lozahenrich@gmail.com>
2022-05-18 14:17:25 +0200

    EC-10 restful warehouse

---
+++ src/main/java/com/microservices/warehouse/Product.java
@@ -0,0 +1,85 @@
+package com.microservices.warehouse;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private Float price;
+    private String description;
+    private String category;
+    private String image;
+    private Float rating;
+    private Integer ratingcount;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public Integer getRatingcount() {
+        return ratingcount;
+    }
+
+    public void setRatingcount(Integer ratingcount) {
+        this.ratingcount = ratingcount;
+    }
+
+}
